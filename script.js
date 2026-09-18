98c65aa32518725cfba0f4bc4b72d04c27928076
saichandu0511 <saichandanreddy226@gmail.com>
2026-09-18 14:41:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let table = document.getElementById("sampleTable");
 
 	let row = (table.insertRow(0));
 
-	let cell1 = row.insertRow(0);
-    let cell2 = row.insertRow(1);
+	let cell1 = row.insertcell(0);
+    let cell2 = row.insertcell(1);
 
 	cell1.innerText="New Cell1";
 	cell2.innerText="New Cell2";
